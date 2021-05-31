49d7acbb0cefba9e15725cdbcebc551b422688fd
dipeshtimilsina <dipesh.nepali@gmail.com>
2021-05-30 21:37:21 -0300

    created components

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '/BusinessList.css';
+import '../Business/Business.js';
+
+class BusinessList extends React.Component{
+    render(){
+        return(
+            <div class="BusinessList">
+                <!-- Add Business Component here --> 
+                <!-- Add Business Component here -->
+                <!-- Add Business Component here -->
+                <!-- Add Business Component here -->
+                <!-- Add Business Component here -->
+                <!-- Add Business Component here -->
+            </div>
+        )
+    }
+}
